d13136c7e619da93533fae817436cfb91b71c85d
BT_1E10_19 <dennism27@nycstudents.net>
2022-03-01 13:04:09 -0500

    Initial commit

---
+++ src/MovieCollectionRunner.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class MovieCollectionRunner
+{
+  public static void main(String[] args)
+  {
+    MovieCollection collection = new MovieCollection("src\\movies.csv");
+    ArrayList<Movie> movieList = collection.getMovies();
+    for (Movie movie : movieList)
+    {
+      System.out.println(movie);
+    }
+    collection.menu();
+  }
+}
